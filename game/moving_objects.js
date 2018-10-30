52cd7eb6fff1e261815e90a877aa147454680830
anthony <anthonyltam@gmail.com>
2018-10-30 11:57:31 -0700

    Moving object created

---
+++ game/moving_objects.js
@@ -0,0 +1,13 @@
+class MovingObject {
+  constructor(props) {
+    this.pos = props.pos;
+    this.vel = props.vel;
+    this.rad = props.rad;
+    this.height = props.height;
+    this.width = props.width;
+    this.color = "rgb(255, 140, 0)";
+  }
+}
+
+
+export default MovingObject;
